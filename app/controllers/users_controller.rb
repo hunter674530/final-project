944cc3768091a85575e77980f5030100d1960daf
Hunter Wood <hunter674530@gmail.com>
2023-03-12 19:24:05 -0600

    Working login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,24 @@
 class UsersController < ApplicationController
-
-  def index
-    user = User.all
-    render json: user, status: :ok
-end
-
-def show
-    user = User.find(params[:id])
-    render json: user, status: :ok
-end
-
-def create
-    user = User.create!(user_params)
-    render json: user, status: :created
-end
-
-def update
-  user = User.find(params[:id])
-  user.update(user_params)
-  render json: user, status: :created
-end
-
-def destroy
-user = User.find(params[:id])
-user.destroy
-render json: user, status: :ok
-end
-
-private
-
-def user_params
-    params.permit(:name, :password, :email_address, :phone_number, :state)
-end
-
-
-end
+  skip_before_action :authorize, only: :create
+  
+      def create
+          # byebug
+          user = User.create!(user_params)
+          session[:user_id] = user.id
+  
+          render json: user, status: :created
+      end
+  
+      def show
+          render json: @current_user 
+          #show me who the current user is 
+      end
+  
+      private
+  
+      def user_params
+          params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+      end
+  end
 
 
